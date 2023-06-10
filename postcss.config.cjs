2b91edd17732d57475aaa005f4746c45caafbaf7
Abolfazl Chaman <88617114+abolfazlchaman@users.noreply.github.com>
2023-06-10 15:49:15 +0430

    added postcss normalize & autoreset

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,12 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: [require("tailwindcss"), require("autoprefixer"), require("postcss-nested")],
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    require("postcss-nested"),
+    require("postcss-normalize")(),
+    require("postcss-autoreset")(),
+  ],
 };
 
 module.exports = config;
